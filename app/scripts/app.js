d07b0a6f61ab08e23ffc93a081fcaf0a5babacab
Uri Shaked <uri@salsa4fun.co.il>
2014-01-24 20:07:11 +0200

    Initial angular.js project for the frontend

---
+++ app/scripts/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('gdgilSiteApp', [
+  'ngResource',
+  'ngRoute'
+])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
