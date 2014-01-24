109f1f87c650784b1af659422b0a80d8ba2123cb
Uri Shaked <uri@salsa4fun.co.il>
2014-01-24 20:16:45 +0200

    Reindent code

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,16 +1,16 @@
 'use strict';
 
 angular.module('gdgilSiteApp', [
-  'ngResource',
-  'ngRoute'
-])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
-  });
+		'ngResource',
+		'ngRoute'
+	])
+	.config(function ($routeProvider) {
+		$routeProvider
+			.when('/', {
+				templateUrl: 'views/main.html',
+				controller: 'MainCtrl'
+			})
+			.otherwise({
+				redirectTo: '/'
+			});
+	});
