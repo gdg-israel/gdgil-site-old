ae816a53e4919a5e00e743a4c7005d920b98a538
Uri Shaked <uri@salsa4fun.co.il>
2014-02-03 21:55:33 +0200

    Move unique filter to a separate file, fix jshint issues

--- app/scripts/controllers/events.js
+++ app/scripts/controllers/events.js
@@ -7,18 +7,5 @@ angular.module('gdgilSiteApp')
 		$scope.$watch('ownerSearch', function () {
 			$scope.locationSearch = null;
 		});
-	})
-	.filter('unique', function () {
-		return function (input, key) {
-			var unique = {};
-			var uniqueList = [];
-			for (var i = 0; i < input.length; i++) {
-				if (typeof unique[input[i][key]] == "undefined") {
-					unique[input[i][key]] = "";
-					uniqueList.push(input[i]);
-				}
-			}
-			return uniqueList;
-		};
-	})
+	});
 
