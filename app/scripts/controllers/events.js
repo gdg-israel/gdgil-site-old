5600e448570dacd74e55732e017763cd4ebe6eb7
Uri Shaked <uri@salsa4fun.co.il>
2014-02-03 21:53:21 +0200

    Fix indent

--- app/scripts/controllers/events.js
+++ app/scripts/controllers/events.js
@@ -1,24 +1,24 @@
 'use strict';
 
 angular.module('gdgilSiteApp')
-    .controller('EventsCtrl', function ($scope, $resource) {
-        $scope.events = $resource('/mock/events.json').query();
+	.controller('EventsCtrl', function ($scope, $resource) {
+		$scope.events = $resource('/mock/events.json').query();
 
-        $scope.$watch('ownerSearch', function () {
-            $scope.locationSearch = null;
-        });
-    })
-    .filter('unique', function () {
-        return function (input, key) {
-            var unique = {};
-            var uniqueList = [];
-            for (var i = 0; i < input.length; i++) {
-                if (typeof unique[input[i][key]] == "undefined") {
-                    unique[input[i][key]] = "";
-                    uniqueList.push(input[i]);
-                }
-            }
-            return uniqueList;
-        };
-    })
+		$scope.$watch('ownerSearch', function () {
+			$scope.locationSearch = null;
+		});
+	})
+	.filter('unique', function () {
+		return function (input, key) {
+			var unique = {};
+			var uniqueList = [];
+			for (var i = 0; i < input.length; i++) {
+				if (typeof unique[input[i][key]] == "undefined") {
+					unique[input[i][key]] = "";
+					uniqueList.push(input[i]);
+				}
+			}
+			return uniqueList;
+		};
+	})
 
